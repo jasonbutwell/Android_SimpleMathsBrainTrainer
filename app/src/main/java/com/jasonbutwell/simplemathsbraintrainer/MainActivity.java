0ea6fceb6e6d157150dbf2401a5d7de6b4819523
Jason Butwell <jasonbutwell@gmail.com>
2016-09-29 09:45:16 +0100

    Created UI elements

---
+++ app/src/main/java/com/jasonbutwell/simplemathsbraintrainer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.jasonbutwell.simplemathsbraintrainer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void start( View view ) {
+        // start the start button to invisible
+        startButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+    }
+}
